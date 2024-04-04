c4d7037dcf26aec41db0f0a6d3996649f0f74809
Damie Pak <dkp5177@psu.edu>
2024-04-04 14:16:33 -0400

    Created an R script for the cophylogeny plots and deleted more scripts that are no longer necessray.

---
+++ Code/Data_Analysis/02_Figure_Cophylogeny.R
@@ -0,0 +1,25 @@
+library(here)
+source(here("Code","Functions", "FUNC_Package_Loader.R"))
+source(here("Code", "Data_Analysis","00_parasite_phylogeny_tree.R"))
+
+
+###Use the subsetted one
+mal_dat_asex <- read.csv(here("Data","MALARIA_PHYLOGENY_HOST_VARIOUS.csv"))
+mal_dat_asex$Host <- add_underscore(mal_dat_asex$Host)
+
+
+association_matrix <-mal_dat_asex[,c("Plasmodium_species", "Host")]
+
+
+###Pruning down the host trees for the analysis as well as making the cophylogenetic
+###figure 
+
+type_host_tree_subsetted <- drop.tip(Full_SuperTree_Host, 
+                                     Full_SuperTree_Host$tip.label
+                                     [-match(mal_dat_asex$Host, Full_SuperTree_Host$tip.label)])
+
+plot(cophylo(plasmodium_tree_full,
+            type_host_tree_subsetted,
+            association_matrix), rotate = TRUE, use.edge.length	 = FALSE)
+
+###THIS REQUIRES A LOT OF MANUAL EDITING IN ILLUSTRATOR
