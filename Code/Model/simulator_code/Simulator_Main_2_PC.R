b159992178a7db95938fd86140394072c6ed98a1
Damie Pak <dkp5177@psu.edu>
2024-04-29 11:46:02 -0400

    Updated the Package Loader as well as cleaned up the structure/commented on the simulating functions for Plasmodium chabaudi and Plasmoidum falciparum.

--- Code/Model/simulator_code/Simulator_Main_2_PC.R
+++ Code/Model/simulator_code/Simulator_Main_2_PC.R
@@ -1,26 +1,21 @@
-##########################
-### Main simulation code##
-##########################
-######################################################
-###Please ensure that the pmax is 4.0e-6 AND that the#
-### initial I (default) is 4358.965            #
-############################################################
+### Title: Main simulation codes for Plasmodium chabaudi
+### Notes: Please ensure that the pmax is 4.0e-6 AND that the
+### initial (default) is 4358.965            #
+###
 ### Input: Burst size (B_V), transmission investment (C_V),#
-### and the initial value (initialvalue)                   #
-### Output: dataframe with only IRBC and Gametocytes       #                
-##                                                         #  
-############################################################
-### Main modeling code
+### and the initial, inoculum of parasite (initialvalue)
+### Output: Dataframe with only IRBC and gametocytes
+
+### Main modeling codes are written in C++, so if you would like to 
+### dive into the mathematics- look here:
 sourceCpp(here("Code", "Model", "rcpp", "rcpp_malaria_dynamics_CUT.cpp"))
 sourceCpp(here("Code", "Model", "rcpp", "rcpp_malaria_dynamics_UNCUT.cpp"))
 
-###This is the function that terminates the integrator early
-### when the red blood cells reaches 6.5 * 10^5
-
+### This function simulates to Day 100 unless there is death.
 Simulator_Malaria_BC_PC <- function(B_V, C_V, initialvalue, include_death) {
-  
   parameters_n <-
-    c(lambda = 370000, # Replenishment rate of RBC (#SimulatedTimeSeries.R)
+    c(
+      lambda = 370000, # Replenishment rate of RBC (#SimulatedTimeSeries.R)
       K = 19968254, # Carrying capacity of RBC population in the absence of mortality
       pmax = 4.0e-6, # Rate of infection (From American Naturalist- Greischar et al. 2014)
       muR = 0.025, # Daily mortality rate of red blood cells
@@ -28,70 +23,73 @@ Simulator_Malaria_BC_PC <- function(B_V, C_V, initialvalue, include_death) {
       c = C_V, # Transmission investment (THE VARYING FACTOR)
       B = B_V, # The burst size (THE VARYING FACTOR)
       alpha1 = 1, # The rate of development of parasite in iRBC
-      alpha2 = 1/2, # The rate of development for the gametocytes
+      alpha2 = 1 / 2, # The rate of development for the gametocytes
       muM = 48, # Background mortality of the merozoite
-      muG = 4, # background mortality of the immature/mature gametocytes
-      n1 = 100, # shape parameter controlling the variability in asexual devleopment
-      n2 = 100 # shape parameter controlling the variability in sexual development
+      muG = 4, # Background mortality of the immature/mature gametocytes
+      n1 = 100, # Shape parameter controlling the variability in asexual devleopment
+      n2 = 100 # Shape parameter controlling the variability in sexual development
     )
-  
-  ### The number of subcompartments for infected rbc(n1)
+
+  ### The number of subcompartments for infected RBCs
   n1 <- parameters_n["n1"]
-  ### The number of subcompartments for immature gametocytes
+  ### The number of subcompartments for Immature Gametocytes
   n2 <- parameters_n["n2"]
-  
+
   ### The initial numbers
   inits_n <-
     c(
-    R = 8500000, # RBC
-    I = rep(initialvalue/ n1, n1), # Infected RBC- note the uniform distribution
-    M = 0, # merozoite
-    IG = rep(0, n2), # immature gametocytes
-    G = 0
-  ) # gametocytes
-  
-  ### We just want to figure out when the peak infected RBC is at.
-  times <- seq(0, 100, by = 1 / 10)
-  
-  
-  rootfun <- function (t, y, parms) {return(y['R'] - 6.5*10^5)}
-  
-  if(include_death == "No"){
-  
-  out_DDE <- ode(
-    y = inits_n,
-    times = times,
-    func = Erlang_Malaria,
-    parms = parameters_n)
-  }
-  
-  else if (include_death == "Yes"){
+      R = 8500000, # RBC
+      I = rep(initialvalue / n1, n1), # Infected RBC: note the uniform distribution
+      M = 0, # Merozoites
+      IG = rep(0, n2), # Immature gametocytes
+      G = 0 # Gametocytes
+    ) 
 
+  ### Daily time steps
+  times <- seq(0, 100, by = 1 / 10)
+
+  ### Mortality function: If the red blood cells hit this threshold,
+  ### end the simulation to this point.
+
+  rootfun <- function(t, y, parms) {
+    return(y["R"] - 6.5 * 10^5)
+  }
+
+  if (include_death == "No") {
+    ### Does not include the rootfun
+    out_DDE <- ode(
+      y = inits_n,
+      times = times,
+      func = Erlang_Malaria,
+      parms = parameters_n
+    )
+  } else if (include_death == "Yes") {
+    # Includes the rootfun
     out_DDE <- ode(
       y = inits_n,
       times = times,
       func = Erlang_Malaria,
       parms = parameters_n,
-      rootfun = rootfun)
+      rootfun = rootfun
+    )
   }
-  
-  
- return(data.frame(out_DDE[, c("time", "R", "G")], 
-                   B_V = B_V, 
-                   C_V = C_V,
-                   initialvalue = initialvalue,
-                   infection_length = 
-                   ifelse(!is.null(attributes(out_DDE)$troot),
-                          attributes(out_DDE)$troot,
-                          NA)))
 
+
+  return(data.frame(out_DDE[, c("time", "R", "G")],
+    B_V = B_V,
+    C_V = C_V,
+    initialvalue = initialvalue,
+    infection_length =
+      ifelse(!is.null(attributes(out_DDE)$troot),
+        attributes(out_DDE)$troot,
+        NA
+      )
+  ))
 }
 
 
-#################################################
-### Input: Burst size and transmission investment##
-### Output: deSolve class                        ##
-##################################################
+### This function simulates to the end of the acute phase, you only
+### run this when you know when the acute phase ends from the prior analyses
 
 Simulator_MalariaPC_DDE_BC_Cut <- function(B_V, C_V, initialvalue, endtime) {
   parameters_n <-
@@ -106,33 +104,33 @@ Simulator_MalariaPC_DDE_BC_Cut <- function(B_V, C_V, initialvalue, endtime) {
       alpha1 = 1, # The rate of development of parasite in iRBC
       alpha2 = 1 / 2, # The rate of development
       muM = 48, # Background mortality of the merozoite
-      muG = 4, # background mortality of the immature/mature gametocytes
-      n1 = 100, # shape parameter controlling the variability in asexual devleopment
-      n2 = 100, # shape parameter controlling the variability in sexual development
-      endtime = endtime # When the asexual replication end
+      muG = 4, # Background mortality of the immature/mature gametocytes
+      n1 = 100, # Shape parameter controlling the variability in asexual devleopment
+      n2 = 100, # Shape parameter controlling the variability in sexual development
+      endtime = endtime # When the acute phase ends (And thus the asexual cycle)
     )
-  
+
   ### The number of subcompartments for infected rbc(n1)
   n1 <- parameters_n["n1"]
   ### The number of subcompartments for immature gametocytes
   n2 <- parameters_n["n2"]
-  
+
   ### The initial numbers
   inits_n <- c(
     R = 8500000,
-    I = rep(initialvalue/ n1, n1),
+    I = rep(initialvalue / n1, n1),
     M = 0,
     IG = rep(0, n2),
     G = 0
   )
-  
+
   times <- seq(0, 100, by = 1 / 10)
-  
+
   out_DDE <- ode(
     y = inits_n, times = times,
     func = Erlang_Malaria_Cut,
     parms = parameters_n
   )
-  
+
   return(data.frame(out_DDE[, c("time", "R", "G")], B_V = B_V, C_V = C_V))
 }
--- Code/Model/simulator_code/Simulator_Main_2_PF.R
+++ Code/Model/simulator_code/Simulator_Main_2_PF.R
@@ -1,129 +1,143 @@
-##########################
-### Main simulation code##
-##########################
-######################################################
-###Please ensure that the pmax is 4.0e-6 AND that the#
-### initial I (default) is 4358.965            #
-############################################################
+### Title: Main simulation codes for Plasmodium falciparum (human)
+###
+###
 ### Input: Burst size (B_V), transmission investment (C_V),#
-### and the initial value (initialvalue)                   #
-### Output: dataframe with only IRBC and Gametocytes       #                
-##                                                         #  
-############################################################
-### Main modeling code
-sourceCpp(here("Code", "Model","rcpp", "rcpp_malaria_dynamics_CUT.cpp"))
-sourceCpp(here("Code", "Model","rcpp", "rcpp_malaria_dynamics_UNCUT.cpp"))
+### and the initial, inoculum of parasite (initialvalue)
+### Output: Dataframe with only IRBC and gametocytes
 
-###This is the function that terminates the integrator early
-### when the red blood cells reaches 7.6 percent of the initial
-###RBC, using the same proportion as the mouse data
+### Main modeling codes are written in C++, so if you would like to
+### dive into the mathematics- look here:
 
+sourceCpp(here("Code", "Model", "rcpp", "rcpp_malaria_dynamics_CUT.cpp"))
+sourceCpp(here("Code", "Model", "rcpp", "rcpp_malaria_dynamics_UNCUT.cpp"))
 
-Simulator_Malaria_BC_PF <- function(B_V, C_V, initialvalue,include_death){
-  rootfun <- function (t, y, parms) {return(y['R'] - 380000)}
-  
-   parameters_n <- c(lambda = 2e5, # replenishment rate of RBC (#SimulatedTimeSeries.R)
-                    K = 6315789, # carrying capacity of RBC population in the absence of mortality (#Simulated Time series)
-                    pmax =  8.35e-6 , # rate of infection (From American Naturalist- Greischar et al. 2014)
-                    muR = 1/120, #Daily mortality rate of red blood cells (SimulatedTimeSeries.R)
-                    muI = 1/120, #Daily mortality rate of infected red blood cells (SimulatedTimeSeries.R)
-                    c = C_V, # transmission investment (Vary)
-                    B = B_V, # the burst size (Vary)
-                    alpha1 = 1/2, # the rate of development of parasite in iRBC (SimulatedTimeSeries.R)
-                    alpha2 = 1/7, #the rate of development (SimulatedTimeSeries.R)
-                    muM = 200, # background mortality of the merozoite (SimulatedTimeSeries.R)
-                    muG = log(2)/2.4, # background mortality of the immature/mature gametocytes (SimulatedTimeSeries.R)
-                    n1= 10, # shape parameter controlling the variability in (SimulatedTimeSeries.R)
-                    n2= 10, # background mortality of the merozoite (SimulatedTimeSeries.R)
-                    a = 0,  #parameter associated with immune clearance (from Megan)
-                    b= 0, #parameter associated with immune clearance (from Megan)
-                    q= 0 #Merozite interference (eyeballed graph from From American Naturalist- Greischar et al. 2014 )
+Simulator_Malaria_BC_PF <- function(B_V, C_V, initialvalue, include_death) {
+  parameters_n <- c(
+    lambda = 2e5, # replenishment rate of RBC (#SimulatedTimeSeries.R)
+    K = 6315789, # carrying capacity of RBC population in the absence of mortality (#Simulated Time series)
+    pmax = 8.35e-6, # rate of infection (From American Naturalist- Greischar et al. 2014)
+    muR = 1 / 120, # Daily mortality rate of red blood cells (SimulatedTimeSeries.R)
+    muI = 1 / 120, # Daily mortality rate of infected red blood cells (SimulatedTimeSeries.R)
+    c = C_V, # transmission investment (Vary)
+    B = B_V, # the burst size (Vary)
+    alpha1 = 1 / 2, # the rate of development of parasite in iRBC (SimulatedTimeSeries.R)
+    alpha2 = 1 / 7, # the rate of development (SimulatedTimeSeries.R)
+    muM = 200, # background mortality of the merozoite (SimulatedTimeSeries.R)
+    muG = log(2) / 2.4, # background mortality of the immature/mature gametocytes (SimulatedTimeSeries.R)
+    n1 = 10, # shape parameter controlling the variability in (SimulatedTimeSeries.R)
+    n2 = 10, # background mortality of the merozoite (SimulatedTimeSeries.R)
+    a = 0, # parameter associated with immune clearance (from Megan)
+    b = 0, # parameter associated with immune clearance (from Megan)
+    q = 0 # Merozite interference (eyeballed graph from From American Naturalist- Greischar et al. 2014 )
   )
-  
-  
-  n1=parameters_n['n1'];
-  n2=parameters_n['n2']
-  
-  inits_n <- c(R = 5e6, 
-               I = rep(initialvalue/n1, n1),
-               M = 0,
-               IG=rep(0,n2),
-               G =0)
-  
-  ### We just want to figure out when the peak infected RBC is at.
+
+  ### The number of subcompartments for infected RBCs
+  n1 <- parameters_n["n1"]
+  ### The number of subcompartments for Immature Gametocytes
+  n2 <- parameters_n["n2"]
+
+  ### The initial numbers
+  inits_n <- c(
+    R = 5e6, # RBC
+    I = rep(initialvalue / n1, n1), # Infected RBC: note the uniform distribution
+    M = 0, # Merozoites
+    IG = rep(0, n2), # Immature gametocytes
+    G = 0 # Gametocytes
+  )
+
+  ### Daily time steps
   times <- seq(0, 100, by = 1 / 10)
-  
-  if(include_death == "No"){
-    
+
+
+  ### Mortality function: If the red blood cells hit this threshold,
+  ### end the simulation to this point.
+  rootfun <- function(t, y, parms) {
+    return(y["R"] - 380000)
+  }
+
+
+
+  if (include_death == "No") {
+    ### Does not include the rootfun
+
     out_DDE <- ode(
       y = inits_n,
       times = times,
       func = Erlang_Malaria,
-      parms = parameters_n)
-  }
-  else{
+      parms = parameters_n
+    )
+  } else {
+    # Includes the rootfun
     out_DDE <- ode(
       y = inits_n,
       times = times,
       func = Erlang_Malaria,
       parms = parameters_n,
-      rootfun = rootfun)
+      rootfun = rootfun
+    )
   }
-  return(data.frame(out_DDE[, c("time", "R", "G")], 
-                    B_V = B_V, 
-                    C_V = C_V,
-                    initialvalue = initialvalue,
-                    infection_length = 
-                      ifelse(!is.null(attributes(out_DDE)$troot),
-                             attributes(out_DDE)$troot,
-                             NA)))
-  
+  return(data.frame(out_DDE[, c("time", "R", "G")],
+    B_V = B_V,
+    C_V = C_V,
+    initialvalue = initialvalue,
+    infection_length =
+      ifelse(!is.null(attributes(out_DDE)$troot),
+        attributes(out_DDE)$troot,
+        NA
+      )
+  ))
 }
+
+### This function simulates to the end of the acute phase, you only
+### run this when you know when the acute phase ends from the prior analyses
 
 
 Simulator_MalariaPF_DDE_BC_Cut <- function(B_V, C_V, initialvalue, endtime) {
   parameters_n <-
-    c(lambda =2e5, # replenishment rate of RBC (#SimulatedTimeSeries.R)
+    c(
+      lambda = 2e5, # replenishment rate of RBC (#SimulatedTimeSeries.R)
       K = 6315789, # carrying capacity of RBC population in the absence of mortality (#Simulated Time series)
-      pmax =  8.35e-6 , # rate of infection (From American Naturalist- Greischar et al. 2014)
-      muR = 1/120, #Daily mortality rate of red blood cells (SimulatedTimeSeries.R)
-      muI =1/120, #Daily mortality rate of infected red blood cells (SimulatedTimeSeries.R)
+      pmax = 8.35e-6, # rate of infection (From American Naturalist- Greischar et al. 2014)
+      muR = 1 / 120, # Daily mortality rate of red blood cells (SimulatedTimeSeries.R)
+      muI = 1 / 120, # Daily mortality rate of infected red blood cells (SimulatedTimeSeries.R)
       c = C_V, # transmission investment (Vary)
       B = B_V, # the burst size (Vary)
-      alpha1 = 1/2, # the rate of development of parasite in iRBC (SimulatedTimeSeries.R)
-      alpha2 = 1/7, #the rate of development (SimulatedTimeSeries.R)
+      alpha1 = 1 / 2, # the rate of development of parasite in iRBC (SimulatedTimeSeries.R)
+      alpha2 = 1 / 7, # the rate of development (SimulatedTimeSeries.R)
       muM = 200, # background mortality of the merozoite (SimulatedTimeSeries.R)
-      muG =  log(2)/2.4, # background mortality of the immature/mature gametocytes (SimulatedTimeSeries.R)
-      n1= 10, # shape parameter controlling the variability in (SimulatedTimeSeries.R)
-      n2= 10, # background mortality of the merozoite (SimulatedTimeSeries.R)
-      a = 0,  #parameter associated with immune clearance (from Megan)
-      b= 0, #parameter associated with immune clearance (from Megan)
-      q= 0, #Merozite interference (eyeballed graph from From American Naturalist- Greischar et al. 2014 )
+      muG = log(2) / 2.4, # background mortality of the immature/mature gametocytes (SimulatedTimeSeries.R)
+      n1 = 10, # shape parameter controlling the variability in (SimulatedTimeSeries.R)
+      n2 = 10, # background mortality of the merozoite (SimulatedTimeSeries.R)
+      a = 0, # parameter associated with immune clearance (from Megan)
+      b = 0, # parameter associated with immune clearance (from Megan)
+      q = 0, # Merozite interference (eyeballed graph from From American Naturalist- Greischar et al. 2014 )
       endtime = endtime
     )
-  
-  
-  n1=parameters_n['n1'];
-  n2=parameters_n['n2']
-  
-  inits_n <- c(R =  5e6, 
-               I = rep(initialvalue/n1, n1),
-               M = 0,
-               IG=rep(0,n2),
-               G =0)
-  
-  
-  ### We just want to figure out when the peak infected RBC is at.
+
+  ### The number of subcompartments for infected RBCs
+  n1 <- parameters_n["n1"]
+  ### The number of subcompartments for Immature Gametocytes
+  n2 <- parameters_n["n2"]
+
+  ### The initial number
+  inits_n <- c(
+    R = 5e6, # RBC
+    I = rep(initialvalue / n1, n1), # Infected RBC: note the uniform distribution
+    M = 0, # Merozoites
+    IG = rep(0, n2), # Immature gametocytes
+    G = 0 # Gametocytes
+  )
+
+  ### Daily time steps
   times <- seq(0, 100, by = 1 / 10)
-  
-  
+
+
   out_DDE <- ode(
     y = inits_n, times = times,
     func = Erlang_Malaria_Cut,
     parms = parameters_n
   )
-  
-  
+
+
   return(data.frame(out_DDE[, c("time", "R", "G")], B_V = B_V, C_V = C_V))
-  
 }
